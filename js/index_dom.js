f0a798394354b687a9b69bb38a727d6bb532538f
kevinmonkada <kevinmonkada@gmail.com>
2023-07-24 14:05:41 -0500

    Keyboard Events - Movements and shortcuts

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,6 @@
 import haburguerMenu from "./dom/menu_hamburguesa.js";
 import reloj from "./dom/reloj_alarma.js";
-
+import { moveBall, shortcuts } from "./dom/teclado.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -13,4 +13,9 @@ d.addEventListener("DOMContentLoaded", (e) => {
 		"stopAudio",
 		"audioReloj"
 	);
+});
+
+d.addEventListener("keydown", (e) => {
+	shortcuts(e);
+	moveBall(e, ".ball", ".stage");
 });
