6d3b87708214177e2eeb923561b5949f3dffd9b8
kevinmonkada <kevinmonkada@gmail.com>
2023-07-25 12:00:04 -0500

    Countdown added plus minor fixes

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,8 @@
+import countdown from "./dom/cuenta_regresiva.js";
 import haburguerMenu from "./dom/menu_hamburguesa.js";
 import reloj from "./dom/reloj_alarma.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
+
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -13,6 +15,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
 		"stopAudio",
 		"audioReloj"
 	);
+	countdown("countdown", "Dec 18, 2023", "Feliz Cumpleaños🎉🎊🪩🥳🎁🍾");
 });
 
 d.addEventListener("keydown", (e) => {
