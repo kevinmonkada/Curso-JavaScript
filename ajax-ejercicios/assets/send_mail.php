cb0ec0abfaaa48a9d95f0e17cfed8cb05f5cdf99
Kevin Moncada <kevinmonkada@gmail.com>
2023-10-06 14:29:06 -0500

    send email update

--- ajax-ejercicios/assets/send_mail.php
+++ ajax-ejercicios/assets/send_mail.php
@@ -1,31 +1,31 @@
 <?php
 if (isset($_POST)) {
-    error_reporting(0);
+  error_reporting(0);
 
-$name=$_POST["name"];
-$email=$_POST["email"];
-$subject=$_POST["subject"];
-$comments=$_POST["comments"];
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $subject = $_POST["subject"];
+  $comments = $_POST["comments"];
 
-$domain=$_SERVER["HTTP_HOST"];
-$to="kevinmonkada@gmail.com";
-$subject_mail="Contacto desde el formulario del sitio $domain.";
-$message ="
-<p>
-Datos enviados desde el formulario del sition<b>$domain</b>
-</p>
-<ul>
-    <li>Nombre: <b>$name</b></li>
-    <li>Email: <b>$email</b></li>
-    <li>Asunto: $subject</li>
-    <li>Comentarios: $comments</li>
-</ul>
-";
-$headers = "MIME-Version: 1.0\r\n" . "Content-Type: text/html; charset=utf-8\r\n" . "From: Envío Automático No Responder <no-reply@$domain>";
+  $domain = $_SERVER["HTTP_HOST"];
+  $to = "kevinmonkada@gmail.com";
+  $subject_mail = "Contacto desde el formulario del sitio $domain.";
+  $message = "
+    <p>
+      Datos enviados desde el formulario del sitio <b>$domain</b>
+    </p>
+    <ul>
+      <li>Nombre: <b>$name</b></li>
+      <li>Email: <b>$email</b></li>
+      <li>Asunto: $subject</li>
+      <li>Comentarios: $comments</li>
+    </ul>
+  ";
+  $headers = "MIME-Version: 1.0\r\n" . "Content-Type: text/html; charset=utf-8\r\n" . "From: Envío Automático No Responder <no-reply@$domain>";
 
-$send_mail = mail($to,$subject_mail,$message,$headers);
+  $send_mail = mail($to, $subject_mail, $message, $headers);
 
-if($send_mail) {
+  if($send_mail) {
     $res = [
       "err" => false,
       "message" => "Tus datos han sido enviados"
@@ -37,9 +37,9 @@ if($send_mail) {
     ];
   }
 
-  //header("Access-Control-Allow-Origin:*");
-  //header("Access-Control-Allow-Origin:https://jonmircha.com");
-  header('Content-type: application/json');
+  //header("Access-Control-Allow-Origin: https://jonmircha.com");
+  header("Access-Control-Allow-Origin: *");
+  header( 'Content-type: application/json' );
   echo json_encode($res);
   exit;
 }
